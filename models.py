f6deec0c6cce19e1189268918527eb522b767923
+ <+>
2025-03-13 12:06:57 +0300

    adds_sales_table

--- models.py
+++ models.py
@@ -1,18 +1,23 @@
 from sqlalchemy import declarative_base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import DateTime, relationship
 
 Base = declarative_base
 
 class Customer(Base):
     __tablename__ = 'customers'
-    id = Column(Integer(Primary_key=True))
+
+    #one to many r/ship with sales
+    id = Column(Integer(), Primary_key=True)
     name = Column(String())
     national_id = Column(Integer())
     credit_status = Column(String())
 
+    sales = relationship("Sale", backref="customer")
+
 class Phone(Base):
     __tablename__ = 'phones'
-    id = Column(Integer(Primary_key=True))
+    id = Column(Integer(), Primary_key=True)
     brand = Column(String())
     model = Column(String())
     price = Column(Integer())
@@ -20,8 +25,23 @@ class Phone(Base):
 
 class Payment(Base):
     __tablename__ = 'payments'
-    id = Column(Integer(Primary_key=True))
-
+    id = Column(Integer(), Primary_key=True)
+    sale_id = Column(Integer(), ForeignKey('sale.id'))
+    amount_paid = Column(Integer())
+    date_paid = Column(Integer())
+    next_due_date = Column(Integer())
+ 
 class Sale(Base):
     __tablename__ = 'sales'
-    
+
+    #one to many(payments)
+    total_price = Column(Integer())
+    deposit_paid = Column(Integer())
+    balance_due = Column(Integer())
+    installment_amount = Column(Integer())
+    status = Column(String())
+
+    customer_id = Column(Integer(), ForeignKey('customer.id'))
+    phone_id = Column(Integer(), ForeignKey('phone.id'))
+
+    payments = relationship("Payment", backref="payment")
